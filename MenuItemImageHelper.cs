afd875cb6a91fb6f253c30154f4b53cebaed39e2
Phexyaa <32746604+Phexyaa@users.noreply.github.com>
2024-01-03 15:48:03 -0500

    made home.razor dynamically load ice cream images in the menu selector element.

--- MenuItemImageHelper.cs
+++ MenuItemImageHelper.cs
@@ -1,18 +1,32 @@
 ﻿public static class MenuItemImageHelper
 {
-    public static List<string> BowlImages = new List<string>()
+    public static Dictionary<IceCreamType, string> BowlImages = new Dictionary<IceCreamType, string>()
     {
-        "Images/Bowls/pistachio-bowl.jpeg",
-        "Images/Bowls/vanilla-bowl.jpeg",
-        "Images/Bowls/strawberry-bowl.jpeg",
-        "Images/Bowls/chocolate-bowl.jpeg"
+        { IceCreamType.Chocolate, "Images/Bowls/pistachio-bowl.jpeg" },
+        { IceCreamType.Vanilla, "Images/Bowls/vanilla-bowl.jpeg" },
+        { IceCreamType.Strawberry, "Images/Bowls/strawberry-bowl.jpeg" },
+        { IceCreamType.Pistachio, "Images/Bowls/chocolate-bowl.jpeg" }
     };
-    public static List<string> ConeImages = new List<string>()
+    public static Dictionary<IceCreamType, string> ConeImages = new Dictionary<IceCreamType, string>()
     {
-        "Images/Cones/pistachio-cone.jpeg",
-        "Images/Cones/vanilla-cone.jpeg",
-        "Images/Cones/strawberry-cone.jpeg",
-        "Images/Cones/chocolate-cone.jpeg"
+        { IceCreamType.Chocolate, "Images/Cones/pistachio-cone.jpeg" },
+        { IceCreamType.Vanilla, "Images/Cones/vanilla-cone.jpeg" },
+        { IceCreamType.Strawberry, "Images/Cones/strawberry-cone.jpeg" },
+        { IceCreamType.Pistachio, "Images/Cones/chocolate-cone.jpeg" }
+    };
+    public static Dictionary<IceCreamType, string> BowlImageNames = new Dictionary<IceCreamType, string>()
+    {
+        { IceCreamType.Chocolate, "Choclate-Bowl" },
+        { IceCreamType.Vanilla, "Vanilla-Bowl" },
+        { IceCreamType.Strawberry, "Strawberry-Bowl" },
+        { IceCreamType.Pistachio, "Pistachio-Bowl" },
+    };
+    public static Dictionary<IceCreamType, string> ConeImageNames = new Dictionary<IceCreamType, string>()
+    {
+        { IceCreamType.Chocolate, "Choclate-Cone" },
+        { IceCreamType.Vanilla, "Vanilla-Cone" },
+        { IceCreamType.Strawberry, "Strawberry-Cone" },
+        { IceCreamType.Pistachio, "Pistachio-Cone" },
     };
 }
 
